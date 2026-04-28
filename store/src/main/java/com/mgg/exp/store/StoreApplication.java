1fc1896372acf08ef14a299a172f080a90d52e28
meihao <meihao@51k1k.com>
2026-04-28 13:40:28 +0800

    feat: init

---
+++ store/src/main/java/com/mgg/exp/store/StoreApplication.java
@@ -0,0 +1,13 @@
+package com.mgg.exp.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+	}
+
+}
